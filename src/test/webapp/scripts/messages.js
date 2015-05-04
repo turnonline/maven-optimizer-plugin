8bc86086e6b02be7d2ea64d767fed957faa4b194
Jozef Pohorelec <pohorelec@comvai.com>
2015-05-04 07:02:27 +0200

    -additional fixes for maven release plugin -copyright fix for javascript files

--- src/test/webapp/scripts/messages.js
+++ src/test/webapp/scripts/messages.js
@@ -1,4 +1,4 @@
-<!--
+/*
   ~ Comvai maven optimizer plugin
   ~ Copyright (C) 2015 Comvai, s.r.o. All Rights Reserved.
   ~
@@ -15,7 +15,7 @@
   ~ You should have received a copy of the GNU Lesser General Public
   ~ License along with this library; if not, write to the Free Software
   ~ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-  -->
+*/
 
 var msg = new Array();
 
